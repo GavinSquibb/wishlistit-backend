72700889eae404384127f6348af61723dbeb9304
Gavin Squibb <gavinsquibb1997@gmail.com>
2023-09-12 22:47:38 +0100

    fix: cors

--- backend/settings.py
+++ backend/settings.py
@@ -41,14 +41,16 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     "wishlistit",
+    "corsheaders",
 ]
 
+ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
